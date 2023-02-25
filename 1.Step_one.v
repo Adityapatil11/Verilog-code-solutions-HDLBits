3f73c21069f532a72424676be6e55f9648e99167
Adityapatil11 <89961588+Adityapatil11@users.noreply.github.com>
2023-02-25 11:18:22 +0530

    Add files via upload

---
+++ 1.Step_one.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/25/2023 11:03:17 AM
+// Design Name: 
+// Module Name: Step one
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_module(
+    output one
+    );
+    assign one =1;
+endmodule

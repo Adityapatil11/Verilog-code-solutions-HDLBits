a0065d71ffbd1b0a0959d3ed8568d00491d10374
Aditya P Patil <adityappatil11@gmail.com>
2023-08-13 00:55:53 +0530

    Delete Vectors directory

--- Vectors/19.Vector5.v
+++
@@ -1,8 +0,0 @@
-module top_module (
-    input a, b, c, d, e,
-    output [24:0] out );//
-    wire [24:0]rep,mix;
-    assign rep={ {5{a}},{5{b}},{5{c}},{5{d}},{5{e}}};
-    assign mix={ 5{a,b,c,d,e}};
-    assign out=~rep^mix;
-endmodule

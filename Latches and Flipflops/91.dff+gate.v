c9a62c51677900a446544a8bb97e3534a8e72beb
Adityapatill11 <adityappatil11@gmail.com>
2023-12-26 21:30:54 +0530

    dec26.2

---
+++ Latches and Flipflops/91.dff+gate.v
@@ -0,0 +1,11 @@
+module top_module (
+    input clk,
+    input in, 
+    output out);
+	wire w;
+    assign w = in ^ out;
+    always @(posedge clk)
+        begin
+            out<=w;
+        end
+endmodule

5669490fd6c3297398541532cf7837cc22be2539
Adityapatill11 <adityappatil11@gmail.com>
2023-12-26 21:49:35 +0530

    dec26.3

---
+++ Latches and Flipflops/97.detect any edge.v
@@ -0,0 +1,12 @@
+module top_module (
+    input clk,
+    input [7:0] in,
+    output [7:0] anyedge
+);
+    reg [7:0] inter;
+    always @(posedge clk)
+        begin
+            inter <= in;
+            anyedge<= inter ^ in;
+        end
+endmodule
